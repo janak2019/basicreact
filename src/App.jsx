2205c4ff4bcc89bd0cace8156a9bd168c04d3042
htmlbasic <withjanak@gmail.com>
2025-08-16 11:41:42 +0545

    css changed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-center">
+    <h1 className="text-3xl font-bold ">
 Hello world!
 </h1>
   )
